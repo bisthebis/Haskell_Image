abe1bba786288cea127c4fe089ee90342ec19f3d
bisthebis <bism25@hotmail.com>
2017-10-27 22:04:04 +0200

    Initial commit - Working checkerboard export

---
+++ src/Lib.hs
@@ -0,0 +1,14 @@
+module Lib
+    ( someFunc
+    ) where
+
+import Graphics.Image
+
+checkerBoard :: Image VS RGB Word8
+checkerBoard = makeImage (256,256) (\(x,y) -> if mod (div x 16 + div y 16) 2 == 0 then PixelRGB 0 0 0 else PixelRGB 255 255 255)
+
+save :: IO ()
+save = writeImageExact BMP [] "./pic.bmp" checkerBoard
+
+someFunc :: IO ()
+someFunc = save
