eee82142ac4a8dbbc5766e1360a860ba0b1d5220
bisthebis <bism25@hotmail.com>
2017-10-28 21:21:06 +0200

    Reafctoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,12 +7,12 @@ import Graphics.Image
 checkerBoard :: Image VS RGB Word8
 checkerBoard = makeImage (256,256) (\(x,y) -> if mod (div x 16 + div y 16) 2 == 0 then PixelRGB 0 0 0 else PixelRGB 255 255 255)
 
-save :: IO ()
-save = writeImageExact BMP [] "./pic.bmp" checkerBoard
+saveBMP :: Image VS RGB Word8 -> FilePath -> IO ()
+saveBMP img path = writeImageExact BMP [] path img
 
 someFunc :: IO ()
 someFunc = do
     displayImage checkerBoard
-    save
+    saveBMP checkerBoard "./pic.bmp"
     getLine
     return ()
